d6418a9a3363fe54d6237a4df8a61443940b4811
author <author@corpmail.com>
2017-03-02 01:02:57 +0800

    success add ffmpeg so

--- app/src/main/java/com/ys/ffmpeg/ffmpegsimple/util/FFmpegUtil.java
+++ app/src/main/java/com/ys/ffmpeg/ffmpegsimple/util/FFmpegUtil.java
@@ -10,13 +10,13 @@ public class FFmpegUtil {
 
     static{
         System.loadLibrary("avcodec");
-        System.loadLibrary("avdevice");
-        System.loadLibrary("avfilter");
+//        System.loadLibrary("avdevice");
+//        System.loadLibrary("avfilter");
         System.loadLibrary("avformat");
         System.loadLibrary("avutil");
-        System.loadLibrary("postproc");
+//        System.loadLibrary("postproc");
         System.loadLibrary("swresample");
         System.loadLibrary("swscale");
-        System.loadLibrary("ysffmpeg.so");
+        System.loadLibrary("ysffmpeg");
     }
 }
--- app/src/main/jni/Android.mk
+++ app/src/main/jni/Android.mk
@@ -7,47 +7,42 @@ LOCAL_PATH := $(call my-dir)
 #FFmpeg library
 include $(CLEAR_VARS)
 LOCAL_MODULE := avcodec
-LOCAL_SRC_FILES := libavcodec.so
+LOCAL_SRC_FILES := libavcodec-57.so
 include $(PREBUILT_SHARED_LIBRARY)
 
-include $(CLEAR_VARS)
-LOCAL_MODULE := avdevice
-LOCAL_SRC_FILES := libavdevice.so
-include $(PREBUILT_SHARED_LIBRARY)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE := avfilter
-LOCAL_SRC_FILES := libavfilter.so
-include $(PREBUILT_SHARED_LIBRARY)
+#include $(CLEAR_VARS)
+#LOCAL_MODULE := avdevice
+#LOCAL_SRC_FILES := libavdevice.so
+#include $(PREBUILT_SHARED_LIBRARY)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := avfilter
-LOCAL_SRC_FILES := libavfilter.so
+LOCAL_SRC_FILES := libavfilter-6.so
 include $(PREBUILT_SHARED_LIBRARY)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := avformat
-LOCAL_SRC_FILES := libavformat.so
+LOCAL_SRC_FILES := libavformat-57.so
 include $(PREBUILT_SHARED_LIBRARY)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := avutil
-LOCAL_SRC_FILES := libavutil.so
+LOCAL_SRC_FILES := libavutil-55.so
 include $(PREBUILT_SHARED_LIBRARY)
 
-include $(CLEAR_VARS)
-LOCAL_MODULE := postproc
-LOCAL_SRC_FILES := libpostproc.so
-include $(PREBUILT_SHARED_LIBRARY)
+#include $(CLEAR_VARS)
+#LOCAL_MODULE := postproc
+#LOCAL_SRC_FILES := libpostproc.so
+#include $(PREBUILT_SHARED_LIBRARY)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := swresample
-LOCAL_SRC_FILES := libswresample.so
+LOCAL_SRC_FILES := libswresample-2.so
 include $(PREBUILT_SHARED_LIBRARY)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := swscale
-LOCAL_SRC_FILES := libswscale.so
+LOCAL_SRC_FILES := libswscale-4.so
 include $(PREBUILT_SHARED_LIBRARY)
 
 #Program
@@ -56,5 +51,5 @@ LOCAL_MODULE := ysffmpeg
 LOCAL_SRC_FILES := com_ys_ffmpeg_ffmpegsimple_util_FFmpegUtil.c
 LOCAL_C_INCLUDES += $(LOCAL_PATH)/include
 LOCAL_LDLIBS := -llog -lz
-LOCAL_SHARED_LIBRARIES := avcodec avdevice avfilter avformat avutil postproc swresample swscale
+LOCAL_SHARED_LIBRARIES := avcodec avfilter avformat avutil swresample swscale
 include $(BUILD_SHARED_LIBRARY)
